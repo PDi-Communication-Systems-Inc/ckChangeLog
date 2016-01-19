9c71cdce214474f7dcbb016b387e0d29d098faf1
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-01-19 09:42:39 -0500

    Changes needed to build under AOSP

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, ckChangeLog/src/main)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/ckChangeLog/src/main/res
+LOCAL_MODULE := de.cketti.library.changelog
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
