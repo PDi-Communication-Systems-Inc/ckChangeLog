524733077ce44af409fa5dfd66d705951252b5c5
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-06-01 14:01:30 -0400

    Just prebuild this one for android 2048

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, ckChangeLog/src/main)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/ckChangeLog/src/main/res
-LOCAL_MODULE := de.cketti.library.changelog
+LOCAL_MODULE := DO_NOT_USE.de.cketti.library.changelog
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
 
